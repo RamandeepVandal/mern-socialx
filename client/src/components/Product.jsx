216e762bcd238075b4c1423b87698e0af367c563
Ramandeep Vandal <106114063+RamandeepVandal@users.noreply.github.com>
2023-08-03 17:59:41 -0700

    Changed the information shown in cards.

--- client/src/components/Product.jsx
+++ client/src/components/Product.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 export const Product = ({ product, showUser }) => {
   return (
-    <div className='col-12 col-sm-12 col-md-12 col-lg-12'>
-        <div className='card p-3 m-3' onClick={() => showUser(product?._id)}>
-            <div className='d-flex justify-content-between'>
-                <p className='fs-3'>{product?.title}</p>
-                <p className='fs-4'>${product?.price}</p>
-            </div>
+    <div className="col-12 col-sm-12 col-md-12 col-lg-12">
+      <div
+        className="card p-3 m-3 product-promo-card"
+        onClick={() => {
+          showUser(product?._id, product);
+        }}
+      >
+        <div className="d-flex justify-content-between">
+          <p className="fs-3">{product?.title}</p>
+          <p className="fs-4">${product?.price}</p>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};

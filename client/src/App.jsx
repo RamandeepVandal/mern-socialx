f16843db0cb14f0720bd539d1548555ebb10c639
Ramandeep Vandal <106114063+RamandeepVandal@users.noreply.github.com>
2023-08-03 17:58:23 -0700

    Added routes for details and posts pages.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,28 @@
 // bootstrap import
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 // react router
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // pages
-import { Home } from './pages/Home';
-import { Login } from './pages/Login';
-import { Register } from './pages/Register';
-import { AddProduct } from './pages/AddProduct';
+import { Home } from "./pages/Home";
+import { Login } from "./pages/Login";
+import { Register } from "./pages/Register";
+import { AddProduct } from "./pages/AddProduct";
+import { Details } from "./pages/Details";
+import { UserPosts } from "./pages/UserPosts";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Login />} />
-        <Route path='/home' element={<Home />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/add' element={<AddProduct />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/add" element={<AddProduct />} />
+        <Route path="/details" element={<Details />} />
+        <Route path="/posts" element={<UserPosts />} />
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;

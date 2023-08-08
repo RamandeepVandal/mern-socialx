d9c4c9c50ade1676f25670c089be54d0072fe537
Ramandeep Vandal <106114063+RamandeepVandal@users.noreply.github.com>
2023-08-08 16:01:57 -0700

    Updated card styles.

--- client/src/components/Product.jsx
+++ client/src/components/Product.jsx
@@ -3,17 +3,26 @@ import { useNavigate } from "react-router-dom";
 
 export const Product = ({ product, showUser }) => {
   return (
-    <div className="col-12 col-sm-12 col-md-12 col-lg-12">
+    <div className="col-12 col-sm-12 col-md-12 col-lg-6 mt-3 mb-3">
       <div
-        className="card m-3 p-3 product-promo-card"
+        className="card p-5 product-promo-card h-100 w-100"
         onClick={() => {
           showUser(product?._id, product);
         }}
       >
-        <div className="d-flex justify-content-between align-items-center">
-          <p className="fs-3 page-text-style-h1">{product?.title}</p>
-          <p className="fs-4 page-text-style-h1">${product?.price}</p>
-        </div>  
+        <div className="card-body d-flex justify-content-center align-items-center mb-3">
+          <img
+            src={product?.imageURL}
+            alt="product"
+            className="card-img-top img-fluid w-50"
+          />
+        </div>
+        <div className="card-footer">
+          <div className="d-flex justify-content-evenly align-items-center">
+            <p className="fs-3 page-text-style-h1">{product?.title}</p>
+            <p className="fs-4 page-text-style-h1">${product?.price}</p>
+          </div>
+        </div>
       </div>
     </div>
   );

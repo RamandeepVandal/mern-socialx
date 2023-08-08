b3ae61eb9cff87b0984bb98d5418d330ab958016
Ramandeep Vandal <106114063+RamandeepVandal@users.noreply.github.com>
2023-08-07 17:11:30 -0700

    Created the update and delete routes.

--- server/Routes/routes.js
+++ server/Routes/routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { registerAccount, getAccounts, loginAccount } = require('../controllers/userControllers');
-const { getAllProducts, addProduct, getProductUser, getUserProducts } = require('../controllers/productControllers');
+const { getAllProducts, addProduct, getProductUser, getUserProducts, deletePost, updatePost } = require('../controllers/productControllers');
 
 // create router
 const router = express.Router();
@@ -14,5 +14,6 @@ router.route('/login').post(loginAccount);
 router.route('/products').get(getAllProducts).post(addProduct);
 router.route('/product/user').post(getProductUser);
 router.route('/user/products').post(getUserProducts)
+router.route('/user/post/:id').delete(deletePost).put(updatePost);
 
 module.exports = router;

f903ec1b2b424a57266ab8d35f92c7116697e678
Ramandeep Vandal <106114063+RamandeepVandal@users.noreply.github.com>
2023-08-08 16:05:41 -0700

    Added good type field to the product model

--- server/models/product.js
+++ server/models/product.js
@@ -17,6 +17,10 @@ const productSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    goodType: {
+        type: String,
+        required: true
+    },
     postUser: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
